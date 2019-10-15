4a75c39c745ae309eabc02e2eed687de8ccf2d7b
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-10-15 11:09:47 +0200

    No need for name and email fields in Support form

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -3,20 +3,6 @@
     <b-container>
       <b-card align="center" header="Ask for Support" header-tag="header" class="mb-2">
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-          <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
-            <b-form-input
-              id="input-1"
-              v-model="form.email"
-              type="email"
-              required
-              placeholder="Enter email"
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-            <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
-          </b-form-group>
-
           <b-form-group id="input-group-3" label="Subject:" label-for="input-3">
             <b-form-input id="input-3" v-model="form.subject" required placeholder="Enter subject"></b-form-input>
           </b-form-group>
@@ -48,8 +34,6 @@ export default {
   data () {
     return {
       form: {
-        email: '',
-        name: '',
         subject: null,
         description: null
       },
@@ -113,8 +97,6 @@ export default {
     onReset (evt) {
       evt.preventDefault()
       // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
       this.form.subject = ''
       this.form.description = ''
       this.form.checked = []
